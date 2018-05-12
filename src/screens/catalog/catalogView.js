4c29285fb0006e2308b792baf8c8e80ead73f37a
Tsetsonis Dimitris <jimtse12@gmail.com>
2018-05-12 20:31:35 +0300

    Responsive update, catalog functionality

--- src/screens/catalog/catalogView.js
+++ src/screens/catalog/catalogView.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import sampleShoe from '../../images/1132.jpg';
 
 import '../../App.css';
 
@@ -8,7 +7,7 @@ import axios from 'axios';
 
 import Footer from '../../components/footer';
 import Header from '../../components/header';
-
+import KokkaLoader from '../../components/kokkaLoader';
 
 
 class Catalog extends Component {
@@ -19,19 +18,29 @@ class Catalog extends Component {
             last_page: null,
             page: 1,
 			isLoading: false
-		}; 
+        }; 
+    }
+
+    async componentWillReceiveProps(nextProps, nextState){
+        await this.setState({isLoading: true});
+        window.scrollTo(0, 0);
+        const slug = (nextProps.match.url).match(/^\/+.*\/(.*)$/);
+        setTimeout(async()=>{await axios.get(slug === null || slug[1] === undefined ? 'http://cms.kokkashoes.tk/api/products' : 'http://cms.kokkashoes.tk/api/products/category/'+slug[1]).then((result)=>{
+            this.setState({products: result.data.data, last_page: result.data.last_page, isLoading: false, page: 1})
+        });}, 2500)
     }
 
     componentDidMount(){
+        window.scrollTo(0, 0);
 		const slug = (this.props.match.url).match(/^\/+.*\/(.*)$/);
-        axios.get(slug === null || slug[1] === undefined ? 'http://cms.kokkashoes.tk/api/products' : 'http://cms.kokkashoes.tk/api/products/category'+slug[1]).then((result)=>{
+        axios.get(slug === null || slug[1] === undefined ? 'http://cms.kokkashoes.tk/api/products' : 'http://cms.kokkashoes.tk/api/products/category/'+slug[1]).then((result)=>{
             this.setState({
                 products: result.data.data,
                 last_page: result.data.last_page, 
 				isLoading: false
 			},()=>{
                 const ele = document.getElementById('kokka-loading')
-                if(ele != undefined){
+                if(ele !== undefined){
                     // fade out
                     setTimeout(() => {
                         ele.classList.add('remove')
@@ -45,15 +54,20 @@ class Catalog extends Component {
         })
     }
 
-    componentDidUpdate(){
-        //const slug = (this.props.match.url).match(/^\/+.*\/(.*)$/);
-        axios.get('http://cms.kokkashoes.tk/api/products?page='+this.state.page).then((result)=>{
-            this.setState({products: result.data.data})
-        });
-    }
 
-    nextPage =()=>{
-        this.setState({page: this.state.page+1}); 
+    nextPage = async() =>{
+        await this.setState({isLoading: true});
+        window.scrollTo(0, 0);
+        setTimeout(async()=>{await axios.get('http://cms.kokkashoes.tk/api/products?page='+(this.state.page+1)).then((result)=>{
+             this.setState({products: result.data.data, page: this.state.page+1, isLoading: false})
+         });}, 2500)
+    }
+    prevPage = async() =>{
+        await this.setState({isLoading: true});
+        window.scrollTo(0, 0);
+        setTimeout(async()=>{await axios.get('http://cms.kokkashoes.tk/api/products?page='+(this.state.page-1)).then(async(result)=>{
+            await this.setState({products: result.data.data, page: this.state.page-1, isLoading: false})
+         });}, 2500)
     }
 
 	render() {
@@ -61,18 +75,17 @@ class Catalog extends Component {
         {
             'all' : 'Όλα',
             'boots-n-booties': 'Μπότες & Μποτάκια',
-            'sandals': 'Σανδάλια',
+            'flat-sandals': 'Σανδάλια',
             'flats': 'Flats',
-            'heeled-sandals': 'Πέδιλα',
+            'platforms-n-heeled-sandals': 'Πλατφόρμες & Πέδιλα',
             'mens-sandals': 'Ανδρικά Σανδάλια',
-            'platforms': 'Πλατφόρμες',
         };
         const url = (this.props.match.url).match(/^\/+.*\/(.*)$/);
-        var {images, products, last_page} = this.state;
+        var {products, last_page} = this.state;
 		return (
 			<div className="main-wrapper site-scroll" style={{ backgroundColor: 'transparent', position: 'initial' }}>
 				<div className="header" style={{ zIndex: 99}}><Header /></div>
-				<div className="catalog-menu">
+				<div className="catalog-menu" style={ this.state.isLoading ? {transform: 'translateY(-110px)'} : {transform: 'translateY(0px)'} }>
                     <ul style={{fontFamily: `'GFS Didot', serif`}}>
 						<li> 
                             <Link to="/catalog" style={{borderBottom: this.props.match.url === '/catalog'?'#212121c7 2px solid' : 'unset'}}>Όλα</Link>
@@ -81,34 +94,32 @@ class Catalog extends Component {
                             <Link to="/catalog/boots-n-booties" style={{borderBottom: this.props.match.url === '/catalog/boots-n-booties'?'#212121c7 2px solid' : 'unset'}}>Μπότες & Μποτάκια</Link>
 						</li>
 						<li>
-                            <Link to="/catalog/sandals" style={{borderBottom: this.props.match.url === '/catalog/sandals'?'#212121c7 2px solid' : 'unset'}}>Σανδάλια</Link>
+                            <Link to="/catalog/flat-sandals" style={{borderBottom: this.props.match.url === '/catalog/flat-sandals'?'#212121c7 2px solid' : 'unset'}}>Σανδάλια</Link>
 						</li>
 						<li>
                             <Link to="/catalog/flats" style={{borderBottom: this.props.match.url === '/catalog/flats'?'#212121c7 2px solid' : 'unset'}}>Flats</Link>
 						</li>
 						<li>
-                            <Link to="/catalog/heeled-sandals" style={{borderBottom: this.props.match.url === '/catalog/heeled-sandals'?'#212121c7 2px solid' : 'unset'}}>Πέδιλα</Link>
+                            <Link to="/catalog/platforms-n-heeled-sandals" style={{borderBottom: this.props.match.url === '/catalog/platforms-n-heeled-sandals'?'#212121c7 2px solid' : 'unset'}}>Πλατφόρμες & Πέδιλα</Link>
 						</li>
 						<li>
                             <Link to="/catalog/mens-sandals" style={{borderBottom: this.props.match.url === '/catalog/mens-sandals'?'#212121c7 2px solid' : 'unset'}}>Ανδρικά Σανδάλια</Link>
 						</li>
-						<li>
-                            <Link to="/catalog/platforms" style={{borderBottom: this.props.match.url === '/catalog/platforms'?'#212121c7 2px solid' : 'unset'}}>Πλατφόρμες</Link>
-						</li>
 					</ul>
-				</div>
+                </div>
+                <KokkaLoader isLoading={this.state.isLoading}/>
 				<div className="category-main">
-                    <div className="category-title" style={{marginLeft: 0}}><span>{ url == '' || url == null ? categories['all']: categories[`${url[1]}`]}</span></div>
+                    <div className="category-title" style={{marginLeft: 0}}><span>{ url === '' || url === null ? categories['all']: categories[`${url[1]}`]}</span></div>
                         <div className="products-grid">
                             {products.map((product, index) =>{
-                                var images = JSON.parse(product.images)
+                                var images = JSON.parse(product.images);
                                 return (
                                 <div key={"container-"+index} className="container">
-                                    <Link key={'img-link-'+index} to={"/product/"+product.slug} className="product-front-img"><img  key={'img-'+index} src={'http://cms.kokkashoes.tk/images/shoes/'+product.product_code+'/'+images[0][0]} /></Link>
+                                    <Link key={'img-link-img-'+index} to={"/product/"+product.slug} className="product-front-img"><img  key={'img-'+index} src={'http://kokkashoes.tk/images/shoes/'+product.product_code+'/'+images[0][0]} alt={"Εικονα για το προιον "+product.name_gr} title={product.name_gr}/></Link>
                                     <div key={'product-name-'+index} className="product-name">
-                                    <h1 key={'product-h1-'+index}>{product.name_gr}</h1>
-                                    <p key={'product-code-p-'+index}><span  key={'product-code-span-'+index}>Κωδικός:</span> {product.product_code}</p>
-                                    <h3 key={'product-price-'+index}>{product.price} ‎€</h3>
+                                    <Link key={'img-link-'+index} to={"/product/"+product.slug} className="product-link"><h1 key={'product-h1-'+index}>{product.name_gr}</h1></Link>
+                                        <p key={'product-code-p-'+index}><span  key={'product-code-span-'+index}>Κωδικός:</span> {product.product_code}</p>
+                                        <h3 key={'product-price-'+index}>{product.price} ‎€</h3>
                                     </div>
                                 </div>
                             )})
@@ -116,7 +127,7 @@ class Catalog extends Component {
                         </div>
                         {last_page >1 && <div className="category-pagination">
                             <ul>
-                                <li onClick={this.nextPage}><i className="fas fa-arrow-left"/> Προηγόυμενο</li><li>Επόμενο <i className="fas fa-arrow-right"/></li>
+                                {this.state.page !== 1 && <li onClick={this.prevPage}><i className="fas fa-arrow-left"/> Προηγόυμενο</li>}{this.state.page !== this.state.last_page && <li onClick={this.nextPage}>Επόμενο <i className="fas fa-arrow-right"/></li>}
                             </ul>
                         </div>}
                     </div>
--- src/screens/index/productsGrid.js
+++ src/screens/index/productsGrid.js
@@ -1,14 +1,8 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
-import sampleShoe from '../../images/1132.jpg';
 import '../../App.css';
 
 class ProductsGrid extends Component {
-  constructor(props){
-    super(props)
-  }
-
-
 	render() {
     var products = this.props.products.slice(0, 4);
 		return (
@@ -17,9 +11,9 @@ class ProductsGrid extends Component {
             var images = JSON.parse(product.images)
             return (
             <div key={"container-"+index} className="container">
-                  <Link key={'img-link-'+index} to={"/product/"+product.slug} className="product-front-img"><img  key={'img-'+index} src={'http://cms.kokkashoes.tk/images/shoes/'+product.product_code+'/'+images[0][0]} /></Link>
+                  <Link key={'img-link-'+index} to={"/product/"+product.slug} className="product-front-img"><img  key={'img-'+index} src={'http://kokkashoes.tk/images/shoes/'+product.product_code+'/'+images[0][0]} alt={"Εικονα για το προιον "+product.name_gr} title={product.name_gr}/></Link>
                 <div key={'product-name-'+index} className="product-name">
-                  <h1 key={'product-h1-'+index}>{product.name_gr}</h1>
+                  <Link key={'img-link-'+index} to={"/product/"+product.slug} className="product-link"><h1 key={'product-h1-'+index}>{product.name_gr}</h1></Link>
                   <p key={'product-code-p-'+index}><span  key={'product-code-span-'+index}>Κωδικός:</span> {product.product_code}</p>
                   <h3 key={'product-price-'+index}>20.99 ‎€</h3>
                 </div>
